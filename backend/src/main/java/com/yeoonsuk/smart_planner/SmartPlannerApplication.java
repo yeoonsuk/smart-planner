cb123c2b13cc7a5a827f26adb59fdfde7fec0d6f
정윤석 <98750248+yeoonsuk@users.noreply.github.com>
2026-07-15 16:56:55 +0900

    Add files via upload

---
+++ backend/src/main/java/com/yeoonsuk/smart_planner/SmartPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.yeoonsuk.smart_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPlannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yeoonsuk/smart_planner/SmartPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoonsuk.smart_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
